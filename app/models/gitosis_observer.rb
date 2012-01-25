f83aa16c66673fd49b92646eff21012d708f3ec6
Reid Vandewiele <marut@cat.pdx.edu>
2012-01-24 17:07:06 -0800

    No longer updating gitosis on observed User events
    Because there doesn't seem to be a reason to, and for large projects it is a
    serious performance problem. Every time a user logs in their "last logged in"
    time is updated, triggering Gitosis::update_repositories. This commit fixes
    that.

--- app/models/gitosis_observer.rb
+++ app/models/gitosis_observer.rb
@@ -1,20 +1,20 @@
 class GitosisObserver < ActiveRecord::Observer
   observe :project, :user, :gitosis_public_key, :member, :role, :repository
-  
+
   def after_save(object) ; update_repositories(object) ; end
   def after_destroy(object) ; update_repositories(object) ; end
-  
+
   protected
-  
+
   def update_repositories(object)
     case object
       when Project: Gitosis::update_repositories(object)
       when Repository: Gitosis::update_repositories(object.project)
-      when User: Gitosis::update_repositories(object.projects)
+      when User: object.logger.info('Would have triggered Gitosis::update_repositories(object.project)')
       when GitosisPublicKey: Gitosis::update_repositories(object.user.projects)
       when Member: Gitosis::update_repositories(object.project)
       when Role: Gitosis::update_repositories(object.members.map(&:project).uniq.compact)
     end
   end
-  
+
 end
